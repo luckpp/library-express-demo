e158c36178eba8a01b84e41ee75b8031d17273b7
luckpp <marius.andritoi@yahoo.com>
2020-03-13 20:56:11 +0200

    separated routes

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bookRouter = require('./src/routes/bookRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -33,71 +33,6 @@ if (templatingEngine === 'static') {
   app.set('views', './src/views');
   app.set('view engine', 'ejs');
 
-  const books = [
-    {
-      title: 'War and Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
-    },
-    {
-      title: 'Les Misérables',
-      genre: 'Historical Fiction',
-      author: 'Victor Hugo',
-      read: false
-    },
-    {
-      title: 'The Time Machine',
-      genre: 'Science Fiction',
-      author: 'H. G. Wells',
-      read: false
-    },
-    {
-      title: 'A Journey into the Center of the Earth',
-      genre: 'Science Fiction',
-      author: 'Jules Verne',
-      read: false
-    },
-    {
-      title: 'The Dark World',
-      genre: 'Fantasy',
-      author: 'Henry Kuttner',
-      read: false
-    },
-    {
-      title: 'The Wind in the Willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Grahame',
-      read: false
-    },
-    {
-      title: 'Life On The Mississippi',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: false
-    },
-    {
-      title: 'Childhood',
-      genre: 'Biography',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
-    }];
-
-  bookRouter.route('/')
-    .get((req, res) => {
-      res.render('books', {
-        title: 'Library',
-        nav: [
-          { link: '/books', title: 'Books' },
-          { link: '/authors', title: 'Authors' }
-        ],
-        books
-      });
-    });
-  bookRouter.route('/single')
-    .get((req, res) => {
-      res.send('hello single book');
-    });
   app.use('/books', bookRouter);
 
   app.get('/', (req, res) => {
