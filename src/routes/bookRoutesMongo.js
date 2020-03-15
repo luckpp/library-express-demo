3a4e050dcc987b28d30b4e747eec3fea850f9189
luckpp <marius.andritoi@yahoo.com>
2020-03-15 12:28:39 +0200

    display books data from MongoDB

--- src/routes/bookRoutesMongo.js
+++ src/routes/bookRoutesMongo.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const debug = require('debug')('app:bookRoutesMongo');
+const { MongoClient } = require('mongodb');
 const booksMock = require('../data/booksMock');
 
 const bookRouter = express.Router();
@@ -8,11 +10,28 @@ function router(nav) {
 
   bookRouter.route('/')
     .get((req, res) => {
-      res.render('bookListView', {
-        title: 'Library',
-        nav,
-        books
-      });
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'library-demo';
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('connected correctly to MongoDB');
+
+          const db = client.db(dbName);
+          const col = await db.collection('books');
+          const books = await col.find().toArray();
+
+          res.render('bookListViewMongo', {
+            title: 'Library',
+            nav,
+            books
+          });
+        } catch (error) {
+          debug(error.stack);
+        }
+        client.close();
+      }());
     });
 
   bookRouter.route('/:id')
